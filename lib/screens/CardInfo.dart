295070c45389af52c9f5320c13b3313a30126be5
= <luis019dell@gmail.com>
2025-04-24 14:08:10 -0600

    modificaciones de card

--- lib/screens/CardInfo.dart
+++ lib/screens/CardInfo.dart
@@ -1,32 +1,80 @@
 import 'package:flutter/material.dart';
 
-class CardInfo extends StatelessWidget{
+class CardInfo extends StatelessWidget {
   const CardInfo({super.key});
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('Widgets review',style: TextStyle(color: Colors.white)
-            )),
-        body: ListView(
-            children: [
-              SizedBox(height: 10),
-              ListTile(leading: Icon(Icons.person),title:Text('CardInfo'),subtitle:Text('Subtitulo de CardInfo')),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(onPressed: null, child: Text('Aceptar')),
-                    TextButton(onPressed: null, child: Text('Cancelar')),
-                  ],
-                ),
-              )
-            ],
-        )
+      appBar: AppBar(title: Text('Card screen')),
+      body: ListView(children: [_customcard()]),
     );
   }
 }
 
+class _customcard extends StatelessWidget {
+  const _customcard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).iconTheme.color;
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 1),
+      child: Card(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            ListTile(
+              leading: Icon(
+                Icons.account_circle,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              title: Text(
+                "Nombre",
+                style: TextStyle(
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+              ),
+              subtitle: Text(
+                "Puesto trabajo lorem ipsum dolor sit amet",
+                style: TextStyle(
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+              ),
+            ),
+            //* otra parte
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Aceptar',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryIconTheme.color,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Cancelar',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryIconTheme.color,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 5),
+          ],
+        ),
+        color: primaryColor,
+      ),
+    );
+  }
+}
